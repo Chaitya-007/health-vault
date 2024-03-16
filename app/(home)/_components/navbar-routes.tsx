8ebc302594024bbf7798d2f973950f9ca6a74e8f
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-16 17:16:37 +0530

    saving the new navbar

--- app/(home)/_components/navbar-routes.tsx
+++ app/(home)/_components/navbar-routes.tsx
@@ -5,6 +5,12 @@ import ModeToggle from "@/components/DarkMode";
 import Link from "next/link";
 import Logo from "@/components/navbar/logo";
 
+const Links = [
+  { name: "Home", href: "/" },
+  { name: "Get Started", href: "/user" },
+  { name: "Contact Us", href: "/contact" },
+];
+
 export const NavbarRoutes = () => {
   const { userId } = useAuth();
 
@@ -12,16 +18,24 @@ export const NavbarRoutes = () => {
     <>
       <div className="flex justify-between items-center w-screen">
         <Logo />
-        <div>
-          <Link href="/dashboard" className="text-navy text-lg">
-            Dashboard
-          </Link>
-        </div>
-        <div className="flex px-2">
+        <div className="flex px-2 items-center">
+          {Links.map((link) => (
+            <div className="mx-4" key={link.href}>
+              <Link href={link.href} key={link.name}>
+                {link.name}
+              </Link>
+            </div>
+          ))}
           <div className="mx-4">
             <ModeToggle />
           </div>
-          {userId ? <UserButton /> : <SignInButton />}
+          {userId ? (
+            <UserButton />
+          ) : (
+            <div className="bg-blue-600 p-3 rounded-xl">
+              <SignInButton />
+            </div>
+          )}
         </div>
       </div>
     </>
