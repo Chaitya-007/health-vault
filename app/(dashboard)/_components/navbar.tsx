665fa501823c7c6822e593c705ac8a7339d3366c
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-16 15:15:35 +0530

    Delete Home component and add new dependencies

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,8 @@
+import { NavbarRoutes } from "@/components/navbar-routes";
+export const Navbar = () => {
+  return (
+    <div className="p-4 border-b h-full flex z-40 bg-white dark:bg-black items-centershadow-sm">
+      <NavbarRoutes />
+    </div>
+  );
+};
