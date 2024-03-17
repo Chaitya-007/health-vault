040aeaed6ec09668a10bd4ef49eae58fcb7d9c0a
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-17 11:16:16 +0530

    Add new dependencies for form validation and date manipulation

--- app/(home)/(dashboard)/user/medical-history/page.tsx
+++ app/(home)/(dashboard)/user/medical-history/page.tsx
@@ -1,13 +1,46 @@
 "use client";
 
 import React, { useState } from "react";
+import { format } from "date-fns";
 
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { CalendarIcon } from "lucide-react";
+import { useAuth, currentUser } from "@clerk/nextjs";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { Toaster, toast } from "sonner";
+import { error } from "console";
 interface MedicalRecord {
-  id: number;
+  id: string;
   date: string;
   diagnosis: string;
   medication: string;
 }
+const MedicalRecordSchema = z.object({
+  date: z.string(),
+  diagnosis: z.string().min(2, "Please Elaborate on the diagnosis"),
+  medication: z.string().min(2, "Please Elaborate on the medication"),
+});
+
+export type MedicalRecordFormSchema = z.infer<typeof MedicalRecordSchema>;
 
 const MedicalHistory: React.FC = () => {
   const [medicalRecords, setMedicalRecords] = useState<MedicalRecord[]>([]);
@@ -18,28 +51,65 @@ const MedicalHistory: React.FC = () => {
   const addMedicalRecord = (record: MedicalRecord) => {
     setMedicalRecords([...medicalRecords, record]);
   };
+  const [date, setDate] = React.useState<Date>();
 
   // Display medical records
   const renderMedicalRecords = () => {
-    return medicalRecords.map((record) => (
-      <div
-        key={record.id}
-        className={`flex justify-between items-center border-b border-gray-200 py-2 px-4 dark:text-white dark:bg-gray-800" : "text-gray-600 dark:bg-white"`}
-      >
-        <span>{record.date}</span>
-        <span className="font-semibold">{record.diagnosis}</span>
-        <span>{record.medication}</span>
-      </div>
-    ));
+    return (
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Diagnosis</TableHead>
+            <TableHead>Medication</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {medicalRecords.map((plan) => (
+            <TableRow key={plan.id}>
+              <TableCell className="font-medium">{plan.date}</TableCell>
+              <TableCell>{plan.diagnosis}</TableCell>
+              <TableCell>{plan.medication}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    );
   };
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid }, //
+  } = useForm<MedicalRecordFormSchema>({
+    resolver: zodResolver(MedicalRecordSchema),
+  });
+
+  const { userId } = useAuth();
+  const processForm: SubmitHandler<MedicalRecordFormSchema> = async (data) => {
+    console.log(data);
+    const response = await fetch("/api/medication", {
+      method: "POST",
+      body: JSON.stringify({ ...data, id: userId! }),
+    });
+    const { success } = await response.json();
+    console.log(success);
+    if (success) {
+      addMedicalRecord({ ...data, id: userId! });
+      toast("Data has been sent!");
+      reset();
+      return;
+    }
+    toast("Something went wrong!");
+    return;
+  };
   return (
-    <div className={`flex justify-center items-start mt-8 dark:text-white`}>
+    <div
+      className={`flex flex-col items-center justify-start mt-8 dark:text-white`}
+    >
       <div className="w-1/2">
         <div className={`rounded-lg shadow-lg p-4 dark:bg-gray-800`}>
-          <h2 className={`text-lg font-semibold mb-4 dark:text-white`}>
-            Medical History
-          </h2>
           <div className="mb-4">
             {/* Dark mode toggle button */}
 
@@ -47,60 +117,80 @@ const MedicalHistory: React.FC = () => {
             <h3 className={`text-md font-semibold mb-2 dark:text-white`}>
               Add New Medical Record
             </h3>
-            <form
-              onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-                e.preventDefault();
-                const target = e.target as typeof e.target & {
-                  date: { value: string };
-                  diagnosis: { value: string };
-                  medication: { value: string };
-                };
-                const date = target.date.value;
-                const diagnosis = target.diagnosis.value;
-                const medication = target.medication.value;
-                addMedicalRecord({
-                  id: medicalRecords.length + 1,
-                  date,
-                  diagnosis,
-                  medication,
-                });
-                // Reset the form directly
-                e.currentTarget.reset();
-              }}
-            >
+            <form onSubmit={handleSubmit(processForm)}>
               <div className="flex items-center">
-                <input
-                  type="date"
-                  name="date"
-                  className="rounded-lg border-gray-300 p-2 mr-2"
-                  required
-                />
-                <input
-                  type="text"
-                  name="diagnosis"
-                  placeholder="Diagnosis"
-                  className="rounded-lg border-gray-300 p-2 mr-2"
-                  required
-                />
-                <input
-                  type="text"
-                  name="medication"
-                  placeholder="Medication"
-                  className="rounded-lg border-gray-300 p-2 mr-2"
-                  required
-                />
-                <button
+                <div className="mr-1">
+                  {/* <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[200px] justify-start text-left font-normal",
+                          !date && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {date ? format(date, "PPP") : <span>Pick a date</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        initialFocus
+                        {...register("date", { required: true })}
+                      />
+                    </PopoverContent>
+                  </Popover> */}
+                  {/* error display if date is not selected */}
+                  <input
+                    type="date"
+                    {...register("date")}
+                    className="p-2 rounded-lg border border-white"
+                  />
+                </div>
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="Diagnosis"
+                    className="rounded-lg border-gray-300 p-2 mr-2"
+                    {...register("diagnosis", { required: true })}
+                  />
+                  {errors.diagnosis && (
+                    <p className="text-red-500 text-sm mt-2">
+                      {errors.diagnosis.message}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="Medication"
+                    className="rounded-lg border-gray-300 p-2 mr-2"
+                    {...register("medication", { required: true })}
+                  />
+                  {errors.medication && (
+                    <p className="text-red-500 text-sm mt-2">
+                      {errors.medication.message}
+                    </p>
+                  )}
+                </div>
+                <Button
                   type="submit"
                   className="bg-blue-500 text-white py-2 px-4 rounded-lg"
                 >
                   Add
-                </button>
+                </Button>
               </div>
             </form>
           </div>
           {/* Render existing medical records */}
-          {renderMedicalRecords()}
         </div>
+      </div>
+      <div className="w-[85vw] mt-20">
+        <h2 className="text-lg font-semibold mb-4">Medical Records</h2>
+        {renderMedicalRecords()}
       </div>
     </div>
   );
