767971be6552e403289986b64ff4f06686f6fa5e
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-17 16:26:19 +0530

    Update appointment counts to 0

--- app/(home)/(dashboard)/user/page.tsx
+++ app/(home)/(dashboard)/user/page.tsx
@@ -33,28 +33,28 @@ const UserPage = () => {
         <h2 className="text-3xl m-4">Status</h2>
         <div className="flex flex-wrap justify-between">
           <div className="w-[40vw] border border-cyan-600 rounded-xl m-4">
-            <h2 className="p-3 text-xl font-bold">2</h2>
+            <h2 className="p-3 text-xl font-bold">0</h2>
             <p className="p-3 text-xl font-bold">New Appointment</p>
             <div className="bg-yellow-300 text-black pl-4 mt-4 text-xl font-bold rounded-b-xl">
               <Link href="/appointments">View Details</Link>
             </div>
           </div>
           <div className="w-[40vw] border border-cyan-600 rounded-xl m-4">
-            <h2 className="p-3 text-xl font-bold">2</h2>
+            <h2 className="p-3 text-xl font-bold">0</h2>
             <p className="p-3 text-xl font-bold">Approved Appointment</p>
             <div className="bg-yellow-300 text-black pl-4 mt-4 text-xl font-bold rounded-b-xl">
               <Link href="/appointments">View Details</Link>
             </div>
           </div>
           <div className="w-[40vw] border border-cyan-600 rounded-xl m-4">
-            <h2 className="p-3 text-xl font-bold">2</h2>
+            <h2 className="p-3 text-xl font-bold">0</h2>
             <p className="p-3 text-xl font-bold">Cancelled Appointment</p>
             <div className="bg-yellow-300 text-black pl-4 mt-4 text-xl font-bold rounded-b-xl">
               <Link href="/appointments">View Details</Link>
             </div>
           </div>
           <div className="w-[40vw] border border-cyan-600 rounded-xl m-4">
-            <h2 className="p-3 text-xl font-bold">2</h2>
+            <h2 className="p-3 text-xl font-bold">0</h2>
             <p className="p-3 text-xl font-bold">Total Appointment</p>
             <div className="bg-yellow-300 text-black pl-4 mt-4 text-xl font-bold rounded-b-xl">
               <Link href="/appointments">View Details</Link>
