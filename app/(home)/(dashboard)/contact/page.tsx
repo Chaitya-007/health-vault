9546c80e13c057496ba647d7ce0632aad5f0d9c8
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-17 12:39:29 +0530

    Add Contact page layout and update package dependencies

--- app/(home)/(dashboard)/contact/page.tsx
+++ app/(home)/(dashboard)/contact/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import ContactRight from "./ContactDesign/ContactRight";
+import EarthCanvas from "@/components/3D/Earth/EarthCanvas";
+import Contact from "@/components/EmailContact/PerfectContact";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <>
+      <main className="pt-32 max-w-full">
+        <ContactRight />
+        <section className="flex lg:flex-row mb-4 lg:mb-16 items-center flex-col">
+          <div className="lg:w-1/2 text-center h-[60vh] lg:block hidden w-full">
+            <EarthCanvas />
+          </div>
+          <div className="lg:w-1/2 w-11/12">
+            <Contact />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default ContactPage;
