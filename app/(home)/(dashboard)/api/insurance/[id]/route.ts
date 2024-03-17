90771a85778d6b79749739cd56147aa4e125de82
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-17 11:53:03 +0530

    Fix parameter in find method

--- app/(home)/(dashboard)/api/insurance/[id]/route.ts
+++ app/(home)/(dashboard)/api/insurance/[id]/route.ts
@@ -5,7 +5,7 @@ export async function GET(req: NextRequest, id: { params: { id: string } }) {
     const MongodbClient = await clientPromise;
     const db = MongodbClient.db("Anantya");
     const collection = db.collection("insurance");
-    const data = await collection.find({ id: id }).toArray();
+    const data = await collection.find({ id: id.params.id }).toArray();
     console.log(data);
     return NextResponse.json(data);
 }
