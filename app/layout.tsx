531051a2943e638af4e15ce4b1e1c10b89eae759
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-18 15:49:48 +0530

    Add NextSSRPlugin and ourFileRouter to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 import "@uploadthing/react/styles.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+
 export const metadata: Metadata = {
   title: "Health Vault",
   description: "Health Vault is a personal health record app.",
@@ -25,6 +30,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
             {children}
           </ThemeProvider>
         </body>
