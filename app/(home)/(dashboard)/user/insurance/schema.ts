3ad4111975d942b3a1844ea4bf6c682f68d5047d
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-20 20:55:01 +0530

    Update insuranceCost validation

--- app/(home)/(dashboard)/user/insurance/schema.ts
+++ app/(home)/(dashboard)/user/insurance/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const InsuranceSchema = z.object({
     name: z.string().min(2, { message: "Name is too short" }),
     company: z.string().min(2, { message: "Company name is too short write the full company info" }),
-    insuranceCost: z.number().min(10000, { message: "Cost is too low" }),
+    insuranceCost: z.number().min(0, { message: "Cost is too low" }),
     benificial: z.string().min(2, { message: "Please write the full benificial info" }),
     policynumber: z.string().min(2, { message: "Please write the full policy number" }),
     claimdate: z.string().min(2, { message: "Please write the full claim date" }),
